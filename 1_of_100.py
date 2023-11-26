f97462e7e25d0b57e52862ab15383d53abf7bcbf
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-26 22:28:35 +0100

    First two task are done

--- 1_of_100.py
+++ 1_of_100.py
@@ -1,3 +1,12 @@
 # fIRST DAY OF MY 100 DAY CHALLENGE
 
+# First task
+print("Hello World")
+
+# Second task
+print("Day 1 - Python Print Function")
+print("The function is declared like this")
+print("print('what to print')")
+
+# Third task
 
