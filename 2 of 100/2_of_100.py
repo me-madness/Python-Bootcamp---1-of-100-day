ae580caf6dd07721c5e99dd02db9913e6c3ebea3
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-28 20:48:17 +0100

    exercise

--- 2 of 100/2_of_100.py
+++ 2 of 100/2_of_100.py
@@ -1,8 +1,16 @@
 # Primitive date type
 print("Hello"[4])
 
+
+# Type Error, Type Checking and Type Conversion
 num_char = len(input("What is your name?"))
 print(type(num_char))
 
 new_num_char = str(num_char)
 print("Your name has " + new_num_char + " character.")
+
+# Interactive coding Exercise- Data types
+two_digit_number = input("Type a two digit number: ")
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+print(a + b)
