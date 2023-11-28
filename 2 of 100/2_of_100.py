bff5c4084a9f431fdebff0f9bee2974cac65b1b7
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-28 20:38:16 +0100

    use type char

--- 2 of 100/2_of_100.py
+++ 2 of 100/2_of_100.py
@@ -2,6 +2,7 @@
 print("Hello"[4])
 
 num_char = len(input("What is your name?"))
-print("Your name has " + num_char + " character.")
-
 print(type(num_char))
+
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " character.")
