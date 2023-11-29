e06f5edb55e0b0061a19811e2a456f719ce4bfe3
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-29 22:37:03 +0100

    if/else statement

--- 3 of 100/3_of_100.py
+++ 3 of 100/3_of_100.py
@@ -1,1 +1,8 @@
 # Coditional Statement
+print("Welcome to the rollrcoaster!")
+height = int(input("What is you heght in cm?"))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to grow taller before you can ride.")    
