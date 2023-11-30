1fa190c74873c806bc2a0fbc138bed3364493ee6
me_madness <p_rangel@abv.bg>
2023-11-30 16:18:24 +0100

    new commit day 3

--- 3 of 100/3_of_100.py
+++ 3 of 100/3_of_100.py
@@ -1,11 +1,18 @@
 # Coditional Statement
-print("Welcome to the rollrcoaster!")
-height = int(input("What is you heght in cm?"))
+print("Welcome to the rollercoaster!")
+height = int(input("What is you height in cm?"))
 
 if height > 120:
     print("You can ride the rollercoaster")
 else:
     print("Sorry, you have to grow taller before you can ride.")    
 
-# Odd and Even Execise
+# Odd and Even Exercise
+number =  int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+    print(f"The number is Odd")
+else:
+    print(f"The number is Even")
+        
     
