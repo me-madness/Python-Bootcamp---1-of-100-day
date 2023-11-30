b4996355a28ae9524ed42bb32ccbaada02c5f663
me_madness <p_rangel@abv.bg>
2023-11-30 18:03:55 +0100

    done with love calculator

--- 3 of 100/3_of_100.py
+++ 3 of 100/3_of_100.py
@@ -159,8 +159,13 @@ v = lower_case_string.count("v")
 e = lower_case_string.count("e")
 
 love = l + o + v + e
-love_score = true + love
-print(f"Your score is {love_score}")
+love_score = int(str(true) + str(love))
 
-
+if (love_score < 10) or (love_score > 90):
+    print(f"Your love score is {love_score} you go together like coke and menthos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your love score is {love_score} you are alright together.")
+else:
+    print(f"Your score is {love_score}") 
+       
 # Day 3 Project Treasure Island 
