b1d17ca1433b62d73f2cbde39cfdac0735c318d7
me_madness <p_rangel@abv.bg>
2023-11-30 16:40:50 +0100

    BMI it's done

--- 3 of 100/3_of_100.py
+++ 3 of 100/3_of_100.py
@@ -32,6 +32,20 @@ else:
     print("Sorry, you have to grow taller before you can ride.") 
 
 # Interactive Coding Exercise BMI        
+height = float(input("Enter your height in m:"))
+weight = float(input("Enter your weight in kg:"))
+result = weight / (height ** 2)
+
+if result < 18.5:
+    print("They are underweight.")
+elif result < 25:
+    print("They have a normal weight.")    
+elif result < 30:
+    print("They are overweight.")    
+elif result < 35:
+    print("They are obese.")
+else:
+    print("They are clinically obese.")        
  
 # Interactive Coding Exercise Lea
 
