8c2f74a7854d53174227fd87a380c2702ea24b3a
me_madness <p_rangel@abv.bg>
2023-12-04 21:57:39 +0100

    done with the banker task

--- 4 of 100/4_of_100.py
+++ 4 of 100/4_of_100.py
@@ -36,4 +36,20 @@ import random
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
 
-namesAsCSV = input("Give me everybody's names separet by a comma. ")
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+
+# Who will pay the bill
+
+import random
+
+name_string = input("Give me everybody's names, seperated by a comma. ")
+names = name_string.split(", ")
+
+
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+print(f"{person_who_will_pay} is going to buy the meal today!")
