b6f96d26d39691bdcbb59caddee7f7dced4bd500
me_madness <p_rangel@abv.bg>
2023-12-05 22:41:15 +0100

    make the position integer

--- 4 of 100/4_of_100.py
+++ 4 of 100/4_of_100.py
@@ -81,7 +81,7 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-horizontal = position[0]
-vertical = position[1]
+horizontal = int(position[0])
+vertical = int(position[1])
 
 map[]
