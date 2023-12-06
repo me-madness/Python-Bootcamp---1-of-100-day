b365da2fb4c33c27bd3edf298e515df11d286156
me_madness <p_rangel@abv.bg>
2023-12-06 21:31:02 +0100

    add images

--- 4 of 100/4_of_100_project_.py
+++ 4 of 100/4_of_100_project_.py
@@ -1,8 +1,11 @@
 # Project Rock Paper Scissors
 
 import random
+game_images = [rock, paper, scissors]
 
 user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+print(game_images[user_choise])
+
 
 computer_choice = random.randint(0, 2)
 print(f"Computer chose {computer_choice}")
