20b4312c8a615293265c2539526bc9f78b962d07
me_madness <p_rangel@abv.bg>
2023-12-06 21:19:54 +0100

    import random

--- 4 of 100/4_of_100_project_.py
+++ 4 of 100/4_of_100_project_.py
@@ -1,3 +1,8 @@
 # Project Rock Paper Scissors
 
-user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+import random
+
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+computer_choice = random.randint(0, 2)
+print(f"Computer chose {computer_choice}")
