7e33ec1161d135f38862488e7afbdb31b7b6c3d6
me_madness <p_rangel@abv.bg>
2023-12-06 21:21:34 +0100

    start with if check

--- 4 of 100/4_of_100_project_.py
+++ 4 of 100/4_of_100_project_.py
@@ -6,3 +6,8 @@ user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for S
 
 computer_choice = random.randint(0, 2)
 print(f"Computer chose {computer_choice}")
+
+if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice > user_choice:
+    print("Computer win!")
