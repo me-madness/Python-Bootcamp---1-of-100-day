358741895c9bb85472353aeeb8552175bd26c3f4
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-10 18:31:37 +0100

    Average height

--- 5 of 100/5_of_100.py
+++ 5 of 100/5_of_100.py
@@ -4,6 +4,11 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + "Pie")
-    
+print(fruits)    
 
-#     
+# Exercise - Average Height  
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)      
