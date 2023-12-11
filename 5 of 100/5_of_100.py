de6a756c76d4c00aba20494146e33cb1eec7d5eb
me_madness <p_rangel@abv.bg>
2023-12-11 17:36:04 +0100

    fizzBuzz

--- 5 of 100/5_of_100.py
+++ 5 of 100/5_of_100.py
@@ -74,12 +74,12 @@ print(total)
 # Exercise - The Fizz Buzz
 
 for number in range(100):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
         print("Fizz")
     elif number % 5 == 0:
         print("Buzz")
-    elif number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
     else:
         print(number)            
  
