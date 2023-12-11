f37934b0a63341716bbc854faa2ca75008b05dbd
me_madness <p_rangel@abv.bg>
2023-12-11 18:11:04 +0100

    begin with day six

---
+++ 6 of 100/6_of_100.py
@@ -0,0 +1,29 @@
+#
+
+
+# Defining and calling python functions
+
+
+
+# The hurdles loop challenge
+
+
+
+# Indentation in Python
+
+
+
+# While Loops
+
+
+
+# Hurdles Challenge using While loops
+
+
+
+# Jumping over hurdles with variables heights
+
+
+
+# Project Escaping the Maze
+# 6_of_100_project_escaping_the_maze
