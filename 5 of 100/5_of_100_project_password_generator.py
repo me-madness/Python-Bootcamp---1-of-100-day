ee3f16d73c289aa1a78877345fee46491a81e0e7
me_madness <p_rangel@abv.bg>
2023-12-11 18:02:30 +0100

    eazy way it's done

--- 5 of 100/5_of_100_project_password_generator.py
+++ 5 of 100/5_of_100_project_password_generator.py
@@ -1,4 +1,4 @@
-# Password Generator
+# password_list Generator
 
 import random 
 
@@ -8,5 +8,37 @@ letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['!','#','$','%','&','(', ')','*','+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
+print("Welcome to the Pypassword_list Generator!")
+nr_letters = int(input("How many letters would you like in your password_list?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+# Easy Level
+
+password = ""
+
+for char in range(nr_letters):
+    password += random.choice(letters)
+
+for char in range(nr_symbols):
+    password += random.choice(symbols)    
+    
+for char in range(nr_numbers):
+    password += random.choice(numbers)    
+    
+print(password)
+
+# Hard Level
+
+password_list = []
+
+for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+    password_list += random.choice(symbols)    
+    
+for char in range(nr_numbers):
+    password_list += random.choice(numbers)    
+    
+print(password_list)    
