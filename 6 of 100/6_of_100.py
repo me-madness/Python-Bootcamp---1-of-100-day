8473ff04a45b4a7d7d1df27cd9ef656fcbfd2a5a
me_madness <p_rangel@abv.bg>
2023-12-12 19:57:11 +0100

    def jump

--- 6 of 100/6_of_100.py
+++ 6 of 100/6_of_100.py
@@ -17,11 +17,18 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
-move()
-turn_left()
-move()
-turn_right()    
+
+def jump():    
+    move()
+    turn_left()
+    move()
+    turn_right()  
+    move()
+    turn_right()
+    move()
+    turn_left()  
+
+
 
 
 # Indentation in Python
