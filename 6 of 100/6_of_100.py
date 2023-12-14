0551aeb44db77e65756826356d6a5fc9d46ffcb4
me_madness <p_rangel@abv.bg>
2023-12-14 17:59:22 +0100

    while loop

--- 6 of 100/6_of_100.py
+++ 6 of 100/6_of_100.py
@@ -45,8 +45,28 @@ print("World")
 
 # While Loops
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
+def jump():    
+    move()
+    turn_left()
+    move()
+    turn_right()  
+    move()
+    turn_right()
+    move()
+    turn_left()  
 
+for step in range(6):
+    jump()
+    
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    jump() 
+    number_of_hurdles -= 1
 
 
 # Hurdles Challenge using While loops
