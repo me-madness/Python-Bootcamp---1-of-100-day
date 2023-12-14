41678de24e39035bb641744d51a4f23889e89968
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-14 21:33:57 +0100

    ok commit

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -10,15 +10,15 @@
 # Step 1 and Solution
 word_list = ["ardvark", "baboon", "camel"]
 
-# TODO-1 - """Randomly choose a word from the word_list and assign it to a variable called chosen_word."""
+# Todo-1 - """Randomly choose a word from the word_list and assign it to a variable called chosen_word."""
 import random
 
 chosen_word = random.choice(word_list)
 
-# TODO-2 - """Ask the use to guess a letter and assign their answer to a variable called guess. Make guess lowercase"""
+# Todo-2 - """Ask the use to guess a letter and assign their answer to a variable called guess. Make guess lowercase"""
 guess = input("Guess a letter: ").lower()
 
-# TODO-3 - """Check if the letter the user guessed (guess) is one of the letter in the chosen_word"""
+# Todo-3 - """Check if the letter the user guessed (guess) is one of the letter in the chosen_word"""
 for letter in chosen_word:
     if letter == guess:
         print("Right")
