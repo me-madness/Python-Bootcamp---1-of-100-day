c3eb1924c44301fbc20137bc96c62e3a6ec35295
me_madness <p_rangel@abv.bg>
2023-12-14 18:09:01 +0100

    done with the project

--- 6 of 100/6_of_100_project_escaping_the_maze.py
+++ 6 of 100/6_of_100_project_escaping_the_maze.py
@@ -1,2 +1,16 @@
 # Escaping The Maze
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+while no at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()            
