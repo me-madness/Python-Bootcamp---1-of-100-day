ce0f176cc11a097155582dcbdffaeb1fed9ad954
me_madness <p_rangel@abv.bg>
2023-12-14 18:57:33 +0100

    new commit before the challenge

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -5,14 +5,37 @@
 # How to break a Complex problem down into a Flow chart
 
 
+# Challenge 1 - Picking a Random word and checking answers 
+
+# Step 1
+word_list = ["ardvark", "baboon", "camel"]
+
+# TODO-1 - """Randomly choose a word from the word_list and assign it to a variable called chosen_word."""
+
+# TODO-2 - """Ask the use to guess a letter and assign their answer to a variable called guess. Make guess lowercase"""
+
+# TODO-3 - """Check if the letter the user guessed (guess) is one of the letter in the chosen_word"""
+
+chosen_word = []
+guess = []
+
+
+# Challenge 2 -
 
 
 
 
+# Challenge 3 -
 
 
 
 
+# Challenge 4 -
+
+
+
+
+# Challenge 5 -
 
 
 
