57e88879a3975d80d961b581d3400b2144193230
me_madness <p_rangel@abv.bg>
2023-12-15 21:04:55 +0100

    Checking if the Player has won

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -51,8 +51,30 @@ for position in range(word_length):
 print(display)
 
 
-## Challenge 3 -
+## Challenge 3 - Checking if the Player has Won
 
+
+import random
+
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+# Testing code
+print(f"Pssst, the solution is {chosen_word}.")
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)    
+
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+    letter = chosen_word(position)
+    if letter == guess:
+        display[position] = letter
+
+print(display)
 
 
 
