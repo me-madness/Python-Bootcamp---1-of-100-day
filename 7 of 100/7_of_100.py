0ef61fd23b0082940a0865ec1eb400a90604ec0a
me_madness <p_rangel@abv.bg>
2023-12-15 21:01:42 +0100

    begin with challenge three

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -33,17 +33,17 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 # Testing code
-print(f"Pssst, the soution is {chosen_word}.")
+print(f"Pssst, the solution is {chosen_word}.")
 
 display = []
-word_lenght = len(chosen_word)
-for _ in range(word_lenght):
+word_length = len(chosen_word)
+for _ in range(word_length):
     display += "_"
 print(display)    
 
 guess = input("Guess a letter: ").lower()
 
-for position in range(word_lenght):
+for position in range(word_length):
     letter = chosen_word(position)
     if letter == guess:
         display[position] = letter
