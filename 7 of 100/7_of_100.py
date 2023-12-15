a1ac18a2d356423762b985bfd88dc87855d7535f
me_madness <p_rangel@abv.bg>
2023-12-15 21:11:32 +0100

    Done with task three

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -65,17 +65,23 @@ display = []
 word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
-print(display)    
+print(display) 
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False   
+while not end_of_game: 
+    guess = input("Guess a letter: ").lower()
 
-for position in range(word_length):
-    letter = chosen_word(position)
-    if letter == guess:
-        display[position] = letter
+    for position in range(word_length):
+        letter = chosen_word(position)
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
-print(display)
-
+    print(display)
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
 
 
 ## Challenge 4 -
