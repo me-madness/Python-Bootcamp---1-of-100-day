41513cf1f3601121ce4868321827dd9200d4dda9
me_madness <p_rangel@abv.bg>
2023-12-15 21:22:32 +0100

    new commit before the end

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -97,7 +97,41 @@ stages = ['''
 
 ''']
 
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+# Testing code
+print(f"Pssst, the solution is {chosen_word}.")
 
+display = []
+word_length = len(chosen_word)
+
+lives = 6
+
+for _ in range(word_length):
+    display += "_"
+print(display) 
+
+end_of_game = False   
+while not end_of_game: 
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word(position)
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")    
+    print(f"{' '.join(display)}")
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+        
+        
 ## Challenge 5 -
 
 
