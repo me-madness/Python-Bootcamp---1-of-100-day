5cf00ac0d36846019e527c97ee3dedc432998456
me_madness <p_rangel@abv.bg>
2023-12-15 21:51:56 +0100

    import two files

--- 7 of 100/7_of_100.py
+++ 7 of 100/7_of_100.py
@@ -179,10 +179,52 @@ while not end_of_game:
     print(stages[lives])    
     
     
-## Challenge 5 -
+## Challenge 5 - Improving the User Experience
+
+import random
+
+from hangman_words import word_list
+from hangman_art import stages
 
 
+chosen_word = random.choice(word_list)
+# Testing code
+print(f"Pssst, the solution is {chosen_word}.")
 
+display = []
+word_length = len(chosen_word)
+
+lives = 6
+
+for _ in range(word_length):
+    display += "_"
+print(display) 
+
+end_of_game = False   
+while not end_of_game: 
+    guess = input("Guess a letter: ").lower()
+
+    # Check guesses position
+    for position in range(word_length):
+        letter = chosen_word(position)
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+            
+    # Check the lives        
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")    
+    print(f"{' '.join(display)}")
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+        
+    print(stages[lives])
+    
 
 ## Hangman Project
 # 7_of_100_project_hangman.py
