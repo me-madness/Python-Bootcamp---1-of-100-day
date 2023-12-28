039839755f5d7688fd220f9df41566b474febb16
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-28 11:24:04 +0100

    done man

--- 8 of 100/8_of_100.py
+++ 8 of 100/8_of_100.py
@@ -39,12 +39,14 @@ greet_with(name = "Rangel", locatiion = "Burgas")
 import math
 
 def paint_calc(height, width, cover):
-    test_h = int(input("Height of wall: "))
-    test_w = int(input("Width of wall: "))
-    coverage = 5
     num_of_cans = (test_h * test_w) / coverage
-    print(f"You'll need {round(num_of_cans)} cans of paint")
+    print(f"You'll need {math.ceil(num_of_cans)} cans of paint.")
     
+    
+    
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
   
 
