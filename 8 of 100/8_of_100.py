e51cd5763d54d20a82825883be22b3477f79f632
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-28 12:33:23 +0100

    the project it's almost done

--- 8 of 100/8_of_100.py
+++ 8 of 100/8_of_100.py
@@ -83,27 +83,60 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your mesage:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        cipher_text = alphabet[new_position]
     print(f"The encoded text is {cipher_text}") 
     
        
 encrypt(plain_text=text, shift_amount=shift)
 
-
-
+        
 # Caesar Cipher Part 2 - Decryption
 
-def caesar_two():
-    print("caesar3")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your mesage:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text = alphabet[new_position]
+    print(f"The encoded text is {cipher_text}") 
     
+       
+encrypt(plain_text=text, shift_amount=shift)
+
+def decrypt(cipher_text, shift_amount):
+    plaint_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plaint_text += alphabet[new_position]
+    print(f"The decoded text is {plaint_text}") 
     
-caesar_two()
+       
+decrypt(cipher_text=text, shift_amount=shift)
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)   
+elif direction == "encode":
+    decrypt(cipher_text=text, shift_amount=shift)
+else:
+    print("The type it's not correct, please try again.")
 
 
 # Caesar Cipher Part 3 - Reorganising our Code
@@ -116,10 +149,6 @@ def caesar_three():
 caesar_three()    
 
 # Caesar Cipher Part 4 - User Experience Improvements _ Final Touches
-def caesar_final(name):
-    print(f"{name}")
-    
-    
-caesar_final(name = "final")
+## 8_of_100_caesar_project.py
 
 
--- 8 of 100/8_of_100_caesar_project.py
+++ 8 of 100/8_of_100_caesar_project.py
@@ -4,18 +4,38 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your mesage:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        cipher_text = alphabet[new_position]
     print(f"The encoded text is {cipher_text}") 
     
        
 encrypt(plain_text=text, shift_amount=shift)
+
+def decrypt(cipher_text, shift_amount):
+    plaint_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plaint_text += alphabet[new_position]
+    print(f"The decoded text is {plaint_text}") 
+    
+       
+decrypt(cipher_text=text, shift_amount=shift)
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)   
+elif direction == "encode":
+    decrypt(cipher_text=text, shift_amount=shift)
+else:
+    print("The type it's not correct, please try again.")
