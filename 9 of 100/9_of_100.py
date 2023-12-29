991e485f477feef3279e229f0cc9cffc2f693dee
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-29 18:08:08 +0100

    dictionary

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -1,18 +1,30 @@
 # Day 9 Task
-# Dictionarie Nesting
+# Dictionary, Nesting
 
 
 # The Python Dictionary Deep Dive 
 
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected", 
-    "Function": "a piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again."
+    "Function": "a piece of code that you can easily call over and over again."
 }
 
+#Retrieving items from dictionary.
 print(programming_dictionary["Bug"])
 
+#Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
 
+#Create an empty dictionary.
+empty_dictionary = {}
+
+#Wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = 
 
 #
 
