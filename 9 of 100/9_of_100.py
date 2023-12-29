9a32aefaadab6340f371b288fad45515f3af4bd8
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-29 18:01:48 +0100

    adding Loop definicion

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -6,7 +6,8 @@
 
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected", 
-    "Function": "a piece of code that you can easily call over and over again."
+    "Function": "a piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again."
 }
 
 print(programming_dictionary["Bug"])
