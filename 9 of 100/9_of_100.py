595fa68c1e29dc19b1269f97b3b5d69b0a63eb54
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-29 17:54:20 +0100

    The Python Dictionary Deep Dive

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -4,7 +4,8 @@
 
 # The Python Dictionary Deep Dive 
 
-
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected", 
+                          "Function": "."}
 
 
 #
