14b7f720c68a84e8035b14ca03b9724ac54f100b
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-29 17:57:28 +0100

    new commit about a dictionary

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -4,8 +4,12 @@
 
 # The Python Dictionary Deep Dive 
 
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected", 
-                          "Function": "."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected", 
+    "Function": "a piece of code that you can easily call over and over again."
+}
+
+
 
 
 #
