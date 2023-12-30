fe940b6d79b0647c8f5b2eb6e6733f288557f9d2
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-30 10:39:49 +0100

    Nesting Dictionary in a list

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -67,6 +67,7 @@ capitals = {
   "Bulgaria": "Plovdiv",
   "Luxembourg": "Luxembourg",
 }
+
 #Nesting a List in a Dictionary
 travel_log = {
   "France": ["Paris", "Lille", "Dijon"],
@@ -78,7 +79,21 @@ travel_log = {
   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
   "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 3}
 }
-#Nesting 
+#Nesting Dictionary in a List 
+travel_log = [
+  {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+    },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 3
+    },
+]
+
+
 
 # Exercise - Dictionary in List
 
