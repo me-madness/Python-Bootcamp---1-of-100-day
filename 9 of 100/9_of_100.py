17a0f4e2948d5bc77cf0da781effdc25688cffbb
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-31 13:19:46 +0100

    start with the exercise

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -93,11 +93,20 @@ travel_log = [
     },
 ]
 
-
-
 # Exercise - Dictionary in List
 
-
+travel_log = [
+  {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+    },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 3
+    },
+]
 
 
 # The Secret Auction Program Instructions and Flow Chart
