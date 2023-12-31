c28856b15f4e1b27141fcde1c523dba47656493c
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-31 16:47:43 +0100

    prepare day 10 calculator project

---
+++ 10 of 100/10_of_100.py
@@ -0,0 +1,44 @@
+# Functions with Outputs
+
+
+
+
+# Multiple Return
+
+
+
+
+
+# Exercise - Days in Mount
+
+
+
+
+
+# Docstring
+
+
+
+
+
+# Calculator Part 1 - Combining Dictionaries and Functions
+
+
+
+
+
+# Print vs Return
+
+
+
+
+
+
+# While Looops, Flags and Recursion
+
+
+
+
+
+# Calculator Finishing Touches and Bug Fixes
+##10_of_100_calculator_project.py
