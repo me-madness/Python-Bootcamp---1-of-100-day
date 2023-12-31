2c04f4de5c39182180de403303da0b23390f7177
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-31 15:48:47 +0100

    exercie dictionary in LIst

--- 9 of 100/9_of_100.py
+++ 9 of 100/9_of_100.py
@@ -98,13 +98,13 @@ travel_log = [
 travel_log = [
   {
     "country": "France", 
-    "cities_visited": ["Paris", "Lille", "Dijon"],
-    "total_visits": 12
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"],
     },
   {
     "country": "Germany",
-    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-    "total_visits": 3
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"],
     },
 ]
 
