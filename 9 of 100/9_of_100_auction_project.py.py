4880c4deb84f3bb9453c8dd8dc9cb6e221374ec5
me-madness <58656549+me-madness@users.noreply.github.com>
2023-12-31 16:25:52 +0100

    almost done

--- 9 of 100/9_of_100_auction_project.py.py
+++ 9 of 100/9_of_100_auction_project.py.py
@@ -1,9 +1,33 @@
 # Auction Program Project
 
 from replit import clear
+#Hints use function clear() to clear the inputs
+
+
 from art import logo
 print(logo)
 
+bids = {}
+bidding_finished = False
 
-name = input("What it's your name?:")
-money = input("What it's your bid?:")
+def find_highest_bidder(bidding_record):
+    #highest_bid = max(bidding_record.values()) if bidding_record else None
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid")        
+        
+        
+        
+while not bidding_finished:
+    name = input("What is your name?")
+    price = input("What is your bid? $")
+    bids[name] = price
+    should_continue = input("Are the any other bidder? Type 'yes' or 'no'.")
+    if should_continue == "no":
+        bidding_finished = True
+    elif should_continue == "yes":
+        clear()    
