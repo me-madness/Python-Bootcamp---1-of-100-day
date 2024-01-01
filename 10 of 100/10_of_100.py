cbe891e60865de8da1acf8d1afd6250e7f428fdd
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-01 19:04:18 +0100

    done

--- 10 of 100/10_of_100.py
+++ 10 of 100/10_of_100.py
@@ -11,9 +11,8 @@ my_function("new")
 def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    
-    print(f"{formated_f_name}{formated_l_name}")  
-format_name("John", "Angela")  
+    return f"{formated_f_name}{formated_l_name}"  
+print(format_name("John", "Angela")  )
 
   
 # Multiple Return
