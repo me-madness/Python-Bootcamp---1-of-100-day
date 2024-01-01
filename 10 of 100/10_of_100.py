45922a2be922e68998ae47eb18bf0f92306f6d13
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-01 19:11:53 +0100

    done with multiple return

--- 10 of 100/10_of_100.py
+++ 10 of 100/10_of_100.py
@@ -18,10 +18,11 @@ print(format_name("John", "Angela"))
 # Multiple Return
 
 def format_name(f_name, l_name):
-    if f_name == "" or l_name "":
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name}{formated_l_name}"  
+    return f"Ressult: {formated_f_name}{formated_l_name}"  
 print(format_name(input("What is your first name? "),
                   input("What is your last name? ")))
 
