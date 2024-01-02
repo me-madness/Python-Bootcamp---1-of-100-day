6f2a843e30f56a22c287c3841f0544df62674786
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-02 13:45:03 +0100

    last commit

--- 10 of 100/10_of_100.py
+++ 10 of 100/10_of_100.py
@@ -50,7 +50,7 @@ def days_in_month(year, month):
   if is_leap(year) == 2:
       return 29
   return month_days[month -1]
-#🚨 Do NOT change any of the code below 
+ 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
