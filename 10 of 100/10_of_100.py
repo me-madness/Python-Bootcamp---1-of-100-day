6944b3bc6bd31914808f735dbb3925d67bdfce4e
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-02 12:43:33 +0100

    begin the docstring task

--- 10 of 100/10_of_100.py
+++ 10 of 100/10_of_100.py
@@ -57,9 +57,18 @@ days = days_in_month(year, month)
 print(days)
 
 
-
 # Docstring
 
+lentgth = len(formatted_name)
+
+def format_name(f_name, l_name):
+  """Take a first and last name and format it 
+  to return the title case version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Ressult: {formated_f_name}{formated_l_name}"  
 
 
 
