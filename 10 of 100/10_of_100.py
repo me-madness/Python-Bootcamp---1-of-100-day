e53493f34f691376304416bc4474c44b3e975eb6
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-02 12:33:39 +0100

    Exercise is done

--- 10 of 100/10_of_100.py
+++ 10 of 100/10_of_100.py
@@ -43,10 +43,11 @@ def is_leap(year):
     return False
 
 
-def days_in_month():
+def days_in_month(year, month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  
-  
+  if is_leap(year) == 2:
+      return 29
+  return month_days[month -1]
 #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
