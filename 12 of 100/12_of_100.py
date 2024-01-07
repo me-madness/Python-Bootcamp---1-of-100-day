325c0e1ac25f88c985ce30383341edc83e7a1df0
me_madness <p_rangel@abv.bg>
2024-01-07 20:46:52 +0100

    new commit day 12

--- 12 of 100/12_of_100.py
+++ 12 of 100/12_of_100.py
@@ -1,5 +1,26 @@
 #Namespaces: Local vs. Global scope
+
+
+
+
+
 #Does Python Have Block Scope
+
+
+
+
+
 #How to Modify a Global Variable
+
+
+
+
+
 #Python Constants and Global Scope
+
+
+
+
+
+
 #Exercise: Project The Number Guessing Game
